95daa22d2b7e2ca0e5409c99cb80eca850c093f0
Khamroz <khamroz_001@mail.ru>
2024-08-02 21:29:46 +0500

    added icons, background image and css file and slightly adjusted the WeatherApplication and WeatherController classes and weather_app.fxml file.

--- src/main/java/uz/kh/weather_desktop_app/WeatherController.java
+++ src/main/java/uz/kh/weather_desktop_app/WeatherController.java
@@ -1,14 +1,22 @@
 package uz.kh.weather_desktop_app;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class WeatherController {
-    @FXML
-    private Label welcomeText;
+    @FXML private ImageView weatherIcon;
+    @FXML private ImageView monIcon;
+    @FXML private ImageView tueIcon;
+    @FXML private ImageView wedIcon;
+    @FXML private ImageView thuIcon;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void initialize() {
+
+        weatherIcon.setImage(new Image(getClass().getResourceAsStream("/uz/kh/weather_desktop_app/icons/clear.png")));
+        monIcon.setImage(new Image(getClass().getResourceAsStream("/uz/kh/weather_desktop_app/icons/cloudy.png")));
+        tueIcon.setImage(new Image(getClass().getResourceAsStream("/uz/kh/weather_desktop_app/icons/location.png")));
+        wedIcon.setImage(new Image(getClass().getResourceAsStream("/uz/kh/weather_desktop_app/icons/rain.png")));
+        thuIcon.setImage(new Image(getClass().getResourceAsStream("/uz/kh/weather_desktop_app/icons/snow.png")));
     }
 }
